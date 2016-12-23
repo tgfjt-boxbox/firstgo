078f262b771c4f723d3e4406a28cf77c2015f4f4
tgfjt <tgfjt.mail@gmail.com>
2016-12-23 17:45:26 +0900

    add views template

--- app/hello.go
+++ app/hello.go
@@ -1,8 +1,12 @@
 package hello
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
@@ -11,15 +15,29 @@ import (
 func init() {
 	http.Handle("/", goji.DefaultMux)
 
-	goji.Get("/", handler)
-	goji.Get("/user", handlerUser)
-	goji.Get("/user/:name", handlerUser)
+	goji.Get("/", homeHandler)
+	goji.Get("/user", userHandler)
+	goji.Get("/user/:name", userHandler)
 }
 
-func handler(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", "/")
+func render(v string, w io.Writer, data map[string]interface{}) {
+	tmpl := template.Must(template.ParseFiles("views/layout.html", v))
+	tmpl.Execute(w, data)
 }
 
-func handlerUser(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s/%v", "/user", c.URLParams["name"])
+func homeHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"Name": "home",
+	}
+	render("views/home.html", w, data)
+}
+
+func userHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"Name": c.URLParams["name"],
+	}
+	ctx := appengine.NewContext(r)
+	log.Debugf(ctx, "NAME: "+c.URLParams["name"])
+
+	render("views/user.html", w, data)
 }
